1889c3f05b24a22e2b610083178975cd53ff3e90
TrotskyuYurii <tro2001@gmail.com>
2023-11-21 22:25:29 +0200

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,32 @@
+class StringBuilder {
+  #initialValue;
+
+  constructor(str) {
+    this.#initialValue = str;
+  }
+
+  getValue() {
+    return this.#initialValue;
+  }
+
+  padStart(str) {
+    this.#initialValue = str + this.#initialValue;
+  }
+
+  padEnd(str) {
+    this.#initialValue += str;
+  }
+
+  padBoth(str) {
+    this.#initialValue = str + this.#initialValue + str;
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
